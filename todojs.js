5791e57e6769bfdfc2f46f6b67862724cd209f7b
Sanjarbek20 <raximberdiyevsanjarbek@gmail.com>
2023-02-16 09:40:04 +0300

    added 3 file

---
+++ todojs.js
@@ -0,0 +1,14 @@
+const btn1=document.getElementById('edit')
+btn1.addEventListener('click', function () {
+    this.style.backgroundColor='white';
+
+}
+)
+const btn2=document.getElementById('complete')
+btn2.addEventListener('click', function () {
+    this.style.backgroundColor='blue'
+})
+const btn3=document.getElementById('delete')
+btn3.addEventListener('click', function () {
+    this.style.overflow='hiddin'
+})
